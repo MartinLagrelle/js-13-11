597761a860f14c8b3bb02f17944c76e3337202aa
Lagrelle Martin <lagrellemartin@gmail.com>
2023-11-13 16:29:59 +0100

    en cours

---
+++ identifiant.js
@@ -0,0 +1,10 @@
+let nom = "Lagrelle"
+let prenom = prompt("entrer un prénom");
+let age = 20
+let taille = 1.80
+
+console.log(nom, prenom, age + 2, taille);
+
+let concat = nom + ", " + prenom + ", " + age + ", " + taille
+
+console.log(concat)
